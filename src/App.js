0b17afa2dec71935235a2365ce07ba4e9d06ce81
flyingnitin <nitin03@nagarro.com>
2020-08-20 21:18:56 +0530

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import {Grid} from '@material-ui/core';
+import youtube from  './api/youtube';
+import { SearchBar, VideoDetail } from './components';
 class App extends React.Component {
+    handleSubmit=async(searchTerm)=>{
+        const response=await youtube.get('search',{
+            params:{
+                part:'snippet',
+                maxResults:5,
+                key:'AIzaSyBR0EnmetcCUK6qM47VJO18mZKQx67kMfw',
+                q:searchTerm,
+            }
+        });
+      console.log(response.data.items);
+    }
     render() {
         return ( 
-        <h1> YouTube Clone App </h1>
+        <Grid justify="center" container spacing={10}>
+            <Grid item xs={12}>
+               <Grid container spacing={10}>
+                   <Grid item xs={12}>
+                    <SearchBar onFormSubmit={this.handleSubmit}/>
+                   </Grid>
+                   <Grid item xs={8}>
+                    <VideoDetail/>
+                   </Grid>
+                   <Grid item xs={4}>
+
+                   </Grid>
+               </Grid>
+            </Grid>
+        </Grid>
         )
     }
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Paper,TextField} from '@material-ui/core';
+class SearchBar extends React.Component{
+    state={
+        searchTerm:'',
+    }
+
+    handleChange=(event)=>{
+        this.setState({
+            searchTerm:event.target.value
+        })
+    }
+    handleSubmit=()=>{
+        const {searchTerm}=this.state;
+        const {onFormSubmit}=this.props;
+
+        onFormSubmit(searchTerm);
+    }
+    render(){
+        return(
+            <Paper elevation={6} style={{padding:'25px'}}>
+                <form onSubmit={this.handleSubmit}>
+                    <TextField fullWidth label="Search..." onChange={this.handleChange}></TextField>
+                </form>
+            </Paper>
+            
+        )
+    }
+}
+export default SearchBar;
